1b84347a5c5a3eea75f9edfb1c45aabf0569c318
Mirniy1van <167573783+Mirniy1van@users.noreply.github.com>
2024-05-04 19:16:14 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'banana', 'meat', 'shugar']
+print('список продуктов: ', my_list)
+print('Первый элемент: ', my_list[0], "Последний элемент: ", my_list[-1])
+my_list.append('milk')
+print('С 3 до последнего: ',my_list[2:])
+print('Измененный список: ', my_list)
+
+my_dict = {'apple':"Яблоко", 'meat':"Мясо", 'milk':"Молоко"}
+print("Словарь: ", my_dict)
+print("Вывод значения для заданного ключа: ", my_dict['apple'])
+my_dict.update({'banana':'Банан'})
+print("Измененный словарь: ", my_dict)
